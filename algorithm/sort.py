364f1b829f438ed2cd3f9f9fc73ad74df2723bee
recnac@foxmail.com <recnac@foxmail.com>
2019-09-22 21:12:57 +0800

    optimize selection_sort

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -233,7 +233,7 @@ most stable in time cost, always O(n^2), and easy understanding, used in small s
 def selection_sort(arr):
     for i in range(len(arr)):
         min_i = i
-        for j in range(i, len(arr)):
+        for j in range(i+1, len(arr)):
             if arr[j] < arr[min_i]:
                 min_i = j
         arr[i], arr[min_i] = arr[min_i], arr[i]
